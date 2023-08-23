179b25c53a4936617a1751638b2c4b306bf4c056
ataturan <ata.turan@gmail.com>
2023-08-23 04:44:31 +0300

    Solution for 1342. Number of Steps to Reduce a Number to Zero in JavaScript

---
+++ js/1342. Number of Steps to Reduce a Number to Zero.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number} num
+ * @return {number}
+ */
+var numberOfSteps = function (num) {
+  let stepsNeededToReduceToZero = 0;
+  while (num > 0) {
+    if (num % 2 == 0) {
+      num = num / 2;
+      stepsNeededToReduceToZero++;
+    } else {
+      num = num - 1;
+      num = num / 2;
+      stepsNeededToReduceToZero++;
+    }
+  }
+  return stepsNeededToReduceToZero;
+};
+
+//console.log(numberOfSteps(10));
+
+//Time Complexity: O(logn)
