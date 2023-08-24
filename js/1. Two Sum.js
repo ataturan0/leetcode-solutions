ea76fd0ed22b146bc0369d7d42304ef6dc58423a
ataturan <ata.turan@gmail.com>
2023-08-24 18:21:29 +0300

    Time complexity O(n) solution for the problem ->  1. Two Sum

--- js/1. Two Sum.js
+++ js/1. Two Sum.js
@@ -3,6 +3,30 @@
  * @param {number} target
  * @return {number[]}
  */
+
+var twoSum = function (nums, target) {
+  const map = new Map(); // Create a Map to store numbers and their indices
+  const result = []; // Initialize an array to store resulting indices
+  for (let i = 0; i < nums.length; i++) {
+    if (map.has(target - nums[i])) {
+      // Check if the complement exists in the map
+      result.push(i); // Push the current index to result
+      result.push(map.get(target - nums[i])); // Push the index of complement to result
+      return result; // Return the result array
+    } else {
+      map.set(nums[i], i); // Add the number and its index to the map
+    }
+
+    // console.log(map);
+    // return result;
+  }
+  return result;
+};
+
+//Time Complexity O(n)
+//console.log(twoSum([1, 2, 3, 4, 5], 9));
+
+/*Time Complexity O(n^2) solution
 var twoSum = function (nums, target) {
   const resultArr = []; // Array to store the resulting indices
   let found = false; // Flag to track if a solution is found
@@ -22,3 +46,4 @@ var twoSum = function (nums, target) {
 };
 
 //console.log(twoSum([1, 2, 3, 4, 5], 9))
+*/
